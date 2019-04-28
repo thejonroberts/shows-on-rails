8d204106bdb6d07560f35a13fce5f553308edcda
Jon Roberts <jon.roberts.8@gmail.com>
2019-04-27 21:37:19 -0500

    use structure.sql for shema dump

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,8 @@ module Shows
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    # use structure.sql
+    config.active_record.schema_format = :sql
   end
 end
