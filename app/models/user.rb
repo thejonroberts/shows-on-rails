fc0f642b20fce002bfeeccaf829239680a713490
Jon Roberts <jon.roberts.8@gmail.com>
2019-05-12 12:25:47 -0500

    layout improvements

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,9 @@ class User < ApplicationRecord
 
   has_one :address, as: :addressable, dependent: :destroy
   accepts_nested_attributes_for :address
+
+  def superuser?
+    # TODO: implement a superuser
+    false
+  end
 end
