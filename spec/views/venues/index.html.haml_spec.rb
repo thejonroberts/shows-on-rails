1964f66cd7ac44b95a96f3fb83149b5a69d744df
Jon Roberts <jon.roberts.8@gmail.com>
2019-08-06 18:05:57 -0500

    fix venue index spec

--- spec/views/venues/index.html.haml_spec.rb
+++ spec/views/venues/index.html.haml_spec.rb
@@ -1,20 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe 'venues/index', type: :view do
-  let!(:users) { create_list(:user_with_address, 5) }
+  let!(:venues) { create_list(:venue, 5) }
 
   before(:each) do
-    assign(:addresses, users.collect(&:addresses).flatten)
+    assign(:venues, venues)
   end
 
   it 'renders a list of venues' do
-    # puts 'ADDRESSES', users.collect(&:addresses).flatten.inspect
     render
-    # save_and_open
-    users.each do |user|
-      user.addresses.each do |address|
-        assert_select 'tr>td', text: address.city
-      end
+
+    venues.each do |venue|
+      expect(rendered).to match(venue.name.to_s)
+      expect(rendered).to match(venue.name.to_s)
+      expect(rendered).to match(venue.name.to_s)
     end
   end
 end
