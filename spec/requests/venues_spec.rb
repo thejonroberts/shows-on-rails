f29b9950ea0a9a5c3d4badb22bacfb5a7462fe9b
Jon Roberts <jon.roberts.8@gmail.com>
2019-08-14 22:31:03 -0500

    spec tidying

--- spec/requests/venues_spec.rb
+++ spec/requests/venues_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe 'Venues', type: :request do
-  describe 'GET /venues' do
-    it 'works! (now write some real specs)' do
-      get venues_path
-      expect(response).to have_http_status(200)
-    end
-  end
+RSpec.xdescribe 'Venues', type: :request do
+  # describe 'GET /venues' do
+  #   it 'works!' do
+  #     get venues_path
+  #     expect(response).to have_http_status(200)
+  #   end
+  # end
 end
--- spec/views/addresses/show.html.haml_spec.rb
+++ spec/views/addresses/show.html.haml_spec.rb
@@ -2,23 +2,23 @@ require 'rails_helper'
 require 'support/text_format_helpers'
 
 RSpec.xdescribe 'addresses/show.html.haml', type: :view do
-  include TextFormatHelpers
+  # include TextFormatHelpers
 
-  let(:address) { create(:address) }
+  # let(:address) { create(:address) }
 
-  before(:each) do
-    @address = address
-  end
+  # before(:each) do
+  #   @address = address
+  # end
 
-  it 'renders attributes in <p>' do
-    render
+  # it 'renders attributes in <p>' do
+  #   render
 
-    expect(rendered).to match(html_regex address.line_one)
-    expect(rendered).to match(html_regex address.line_two)
-    expect(rendered).to match(html_regex address.city)
-    expect(rendered).to match(html_regex address.state)
-    expect(rendered).to match(html_regex address.country)
-    expect(rendered).to match(html_regex address.country_code)
-    expect(rendered).to match(html_regex address.zip_code)
-  end
+  #   expect(rendered).to match(html_regex address.line_one)
+  #   expect(rendered).to match(html_regex address.line_two)
+  #   expect(rendered).to match(html_regex address.city)
+  #   expect(rendered).to match(html_regex address.state)
+  #   expect(rendered).to match(html_regex address.country)
+  #   expect(rendered).to match(html_regex address.country_code)
+  #   expect(rendered).to match(html_regex address.zip_code)
+  # end
 end
