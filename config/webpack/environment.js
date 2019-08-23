d2199a47efea4495b0c62a51bf3a5bbdb5a87ad4
Jon Roberts <jon.roberts.8@gmail.com>
2019-08-22 21:39:06 -0500

    webpacker erb loaders

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,5 @@
+const erb =  require('./loaders/erb');
 const { environment } = require('@rails/webpacker')
 
+environment.loaders.prepend('erb', erb)
 module.exports = environment
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   it { is_expected.to validate_presence_of(:email) }
+  it { is_expected.to validate_uniqueness_of(:email) }
   it { is_expected.to validate_presence_of(:display_name) }
   it { is_expected.to have_many :addresses }
   it { is_expected.to accept_nested_attributes_for(:addresses) }
