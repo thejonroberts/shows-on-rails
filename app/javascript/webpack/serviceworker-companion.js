13fc28af30b096eda2cdefd213ccf93ff5de89b4
Jon Roberts <jon.roberts.8@gmail.com>
2019-08-24 16:36:44 -0500

    serviceworkers... working!

---
+++ app/javascript/webpack/serviceworker-companion.js
@@ -0,0 +1,7 @@
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register('/serviceworker.js', { scope: './' })
+    .then(function(reg) {
+      console.log('[Companion]', 'Service worker registered!');
+    })
+    .catch(e => console.error(e));
+}
